ac1fc3fdd513f831b77dcc6aaecbd55bd4b79902
Veronica Soloviova <nika.sv.work@gmail.com>
2023-02-01 02:14:07 +0200

    adds function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+renderGalleryItems(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+}
+
+function renderGalleryItems(galleryItems) {
+    galleryList.innerHTML = createGalleryItemsMarkup(galleryItems);
+}
+
+//console.log(createGalleryItemsMarkup(galleryItems));
+//console.log(galleryList)
